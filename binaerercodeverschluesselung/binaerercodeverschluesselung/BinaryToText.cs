36eaabe92448972a4b983b617203b36867a3c3ac
Ronja Nilsson <ronja.nilsson@autismuslink.ch>
2025-03-03 11:11:51 +0100

    this branch is done

--- binaerercodeverschluesselung/binaerercodeverschluesselung/BinaryToText.cs
+++ binaerercodeverschluesselung/binaerercodeverschluesselung/BinaryToText.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using binaerercodeverschluesselung;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,17 @@ namespace binaerercodeverschluesselung_RoNi
     {
         public BinaryToText()
         {
+            int UserInput = Convert.ToInt32(Console.ReadLine());
             int[] binarynumbers = { 128, 64, 32, 16, 8, 4, 2, 1 };  //make structur to assign value of binary system according to input by user.
+            
+            for (int i = 0; i < binarynumbers.Length; i++) ;
+            {
+                if (binarynumbers.Contains(UserInput))
+                {
+                    Console.WriteLine(ConsoleOutput.GeneratedText);
+                }
+                else { }
+            }
         }
        
     }
