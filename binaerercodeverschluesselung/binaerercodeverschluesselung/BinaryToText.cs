d76700ca4fb4f2f48a7b13aca4e52695956cd59d
Ronja Nilsson <ronja.nilsson@autismuslink.ch>
2025-03-03 15:16:55 +0100

    refurbished

--- binaerercodeverschluesselung/binaerercodeverschluesselung/BinaryToText.cs
+++ binaerercodeverschluesselung/binaerercodeverschluesselung/BinaryToText.cs
@@ -12,14 +12,15 @@ namespace binaerercodeverschluesselung_RoNi
         public BinaryToText(string binTxt)
         {
             int UserInput = Convert.ToInt32(Console.ReadLine());
-            string BinTxt;
+            
             int[] binarynumbers = { 128, 64, 32, 16, 8, 4, 2, 1 };  //make structur to assign value of binary system according to input by user.
             
             for (int i = 0; i < binarynumbers.Length; i++) ;
             {
-               if (UserInput / binarynumbers[i] > 1)
+                int i = 0;
+                if (UserInput / binarynumbers[i] > 1)
                 {
-                    binTxt + = 1;
+                    binTxt += 1;
                 }
                else { }
             }
