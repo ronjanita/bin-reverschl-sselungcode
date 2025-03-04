4f6d1a5597da658dec85f10dc331f237e45d4b84
Ronja Nilsson <ronja.nilsson@autismuslink.ch>
2025-03-04 09:25:09 +0100

    ConsoleOutput finished

--- binaerercodeverschluesselung/binaerercodeverschluesselung/ConsoleOutput.cs
+++ binaerercodeverschluesselung/binaerercodeverschluesselung/ConsoleOutput.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using binaerercodeverschluesselung_RoNi;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +9,23 @@ namespace binaerercodeverschluesselung
 {
     class ConsoleOutput
     {
-        public static void GeneratedText()
+        public static void GeneratedText(BinaryToText binaryToText, TextToBinary textToBinary)
         {
-
+            Console.WriteLine("Welcome to this binary encryption/ decryption program!");
+            Console.WriteLine("Would you like to encrypt(E) or decrypt(D) ?");
+            string EncDec = Console.ReadLine();
+            if (EncDec == "E")
+            {
+                Console.WriteLine($"Your generated ecryption is: {binaryToText} ");
+            }
+            if (EncDec == "D")
+            {
+                Console.WriteLine($"Your generated decryption is {textToBinary} ");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. please try again.");
+            }
         }
     }
 }
