1d9e8772cfdb8fbcbff2f31c2133830c420768df
ronjanita <ronja.nilsson@autismuslink.ch>
2025-03-06 11:55:47 +0100

    changed shortcode of 2 into longer form due to more beginnerfriendly codestructure and improvement of self written code instead of copying code from online

--- binaerercodeverschluesselung/binaerercodeverschluesselung/BinaryToText.cs
+++ binaerercodeverschluesselung/binaerercodeverschluesselung/BinaryToText.cs
@@ -1,4 +1,28 @@
-﻿namespace binaerercodeverschluesselung_RoNi
+﻿//namespace binaerercodeverschluesselung_RoNi
+//{
+//    internal class BinaryToText
+//    {
+//        public string converted = "";
+
+//        public BinaryToText(string binTxtUserInput)
+//        {
+//            for (int startIndexByte = 0; startIndexByte < binTxtUserInput.Length; startIndexByte += 8)   //in 8er schritten durch eingegeben code gehen
+//            {
+//                string bytePlaceHolder = ""; // Ein leerer String für jedes 8-Bit-Set
+//                for (int currentByteIndex = 0; currentByteIndex < 8; currentByteIndex++)  //8 byte für jedes zeicehgn ---> die einzelnen bits ewrden zu bytePlayeHolde3r hinzugefügt
+//                {
+//                    if (startIndexByte + currentByteIndex < binTxtUserInput.Length)
+//                    {
+//                        bytePlaceHolder += binTxtUserInput[startIndexByte + currentByteIndex];
+//                    }                                                                              //brechnet den aktueöen index im gesamten binärstring und stellt sicher das der idex nicht ausserhalb des strings liegt. Wenn index gültig ist, wird er zum bytePlayeHolder hinzugefü¨gt
+//                }
+//                int byteValueInAscii = Convert.ToInt32(bytePlaceHolder, 2);         //konvertiert bytePlaceHolder in eine Zahl (byteValueAscii) und 2 braucht man damnit man weiss das es ich  um eine binär zahl handlet
+//                converted += (char)byteValueInAscii;   //wandelt byteValueAscii in den converted text um wlecher dan ausgegeben wird.
+//            }
+//        }
+//    }
+//}
+namespace binaerercodeverschluesselung_RoNi
 {
     internal class BinaryToText
     {
@@ -6,18 +30,20 @@
 
         public BinaryToText(string binTxtUserInput)
         {
-            for (int startIndexByte = 0; startIndexByte < binTxtUserInput.Length; startIndexByte += 8)   //in 8er schritten durch eingegeben code gehen
+            int[] binarynumbers = { 128, 64, 32, 16, 8, 4, 2, 1 };
+
+            for (int startIndexByte = 0; startIndexByte < binTxtUserInput.Length; startIndexByte += 8)
             {
-                string bytePlaceHolder = ""; // Ein leerer String für jedes 8-Bit-Set
-                for (int currentByteIndex = 0; currentByteIndex < 8; currentByteIndex++)  //8 byte für jedes zeicehgn ---> die einzelnen bits ewrden zu bytePlayeHolde3r hinzugefügt
+                int byteValueInAscii = 0;
+
+                for (int currentByteIndex = 0; currentByteIndex < 8; currentByteIndex++)
                 {
-                    if (startIndexByte + currentByteIndex < binTxtUserInput.Length)
+                    if (startIndexByte + currentByteIndex < binTxtUserInput.Length && binTxtUserInput[startIndexByte + currentByteIndex] == '1')
                     {
-                        bytePlaceHolder += binTxtUserInput[startIndexByte + currentByteIndex];
-                    }                                                                              //brechnet den aktueöen index im gesamten binärstring und stellt sicher das der idex nicht ausserhalb des strings liegt. Wenn index gültig ist, wird er zum bytePlayeHolder hinzugefü¨gt
+                        byteValueInAscii += binarynumbers[currentByteIndex];
+                    }
                 }
-                int byteValueInAscii = Convert.ToInt32(bytePlaceHolder, 2);         //konvertiert bytePlaceHolder in eine Zahl (byteValueAscii) und 2 braucht man damnit man weiss das es ich  um eine binär zahl handlet
-                converted += (char)byteValueInAscii;   //wandelt byteValueAscii in den converted text um wlecher dan ausgegeben wird.
+                converted += (char)byteValueInAscii;
             }
         }
     }
--- binaerercodeverschluesselung/binaerercodeverschluesselung/TextToBinary.cs
+++ binaerercodeverschluesselung/binaerercodeverschluesselung/TextToBinary.cs
@@ -20,6 +20,22 @@
 //        }
 //    }
 //}
+//namespace binaerercodeverschluesselung_RoNi
+//{
+//    internal class TextToBinary
+//    {
+//        public string converted = "";
+
+//        public TextToBinary(string userInput)
+//        {
+//            foreach (char currentSymbol in userInput)      //foreach macht das durch jedes zeichen von userInput durchegehen wird, currentSymbol speichert jedes Zeichen des Textes einzel
+//            {
+//                string convertedToBinary = Convert.ToString(currentSymbol, 2).PadLeft(8, '0');  //convert to string wandelt das currentSymbol in eine binärzahl um, die 2 ist dazu da dass die zahl im binären system dargestellt werden solll. Padleft ist dazu da das ergebniss immer 8 stellen haben soll und immer links noch auffülen mit 0 bisa 8
+//                converted += convertedToBinary + " ";    //wandelt text zu binär um damit das richtige ausgegeben wird.
+//            }
+//        }
+//    }
+//}
 namespace binaerercodeverschluesselung_RoNi
 {
     internal class TextToBinary
@@ -28,12 +44,31 @@ namespace binaerercodeverschluesselung_RoNi
 
         public TextToBinary(string userInput)
         {
-            foreach (char currentSymbol in userInput)      //foreach macht das durch jedes zeichen von userInput durchegehen wird, currentSymbol speichert jedes Zeichen des Textes einzel
+            int[] binarynumbers = { 128, 64, 32, 16, 8, 4, 2, 1 };
+
+            foreach (char currentSymbol in userInput)
             {
-                string convertedToBinary = Convert.ToString(currentSymbol, 2).PadLeft(8, '0');  //convert to string wandelt das currentSymbol in eine binärzahl um, die 2 ist dazu da dass die zahl im binären system dargestellt werden solll. Padleft ist dazu da das ergebniss immer 8 stellen haben soll und immer links noch auffülen mit 0 bisa 8
-                converted += convertedToBinary + " ";    //wandelt text zu binär um damit das richtige ausgegeben wird.
+                string convertedToBinary = "";
+
+                int byteValueInAscii = (int)currentSymbol; // ASCII-Wert des Zeichens holen
+
+                for (int bitPosition = 0; bitPosition < 8; bitPosition++) // dazuzu da um zu prüfen wleche bytes in de rbinärstellung gesetzt werden müssen
+                {
+                    if (byteValueInAscii >= binarynumbers[bitPosition])
+                    {
+                        convertedToBinary += "1";
+                        byteValueInAscii -= binarynumbers[bitPosition];
+                    }
+                    else
+                    {
+                        convertedToBinary += "0";
+                    }
+                }
+
+                converted += convertedToBinary + " ";
             }
         }
     }
 }
+
 
